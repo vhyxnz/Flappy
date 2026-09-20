c7e29cae9de98ec7336cd56e71a1b8e9c6eea81e
vhyxnz <vhyxnz@gmail.com>
2026-09-20 17:54:04 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE='flappy-v4';
+const CORE=[
+  './','./index.html','./game.css','./game.js','./manifest.webmanifest',
+  './logo.png','./bird-up.png','./bird-mid.png','./bird-down.png',
+  './icon-180.png','./icon-192.png','./icon-512.png','./icon-maskable-512.png'
+];
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
+    const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;
+  }).catch(()=>event.request.mode==='navigate'?caches.match('./index.html'):undefined)));
+});
