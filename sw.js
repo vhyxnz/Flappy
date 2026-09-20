0b2b76f7b136af8ce2da5e68acde03400bf026e8
vhyxnz <vhyxnz@gmail.com>
2026-09-20 18:05:28 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='flappy-v4';
+const CACHE='flappy-v6';
 const CORE=[
   './','./index.html','./game.css','./game.js','./manifest.webmanifest',
   './logo.png','./bird-up.png','./bird-mid.png','./bird-down.png',
